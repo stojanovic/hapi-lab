d81358085ba280f2c40998f5d37755fb36cb683f
simalexan <alexander.simovic@gmail.com>
2016-02-18 11:33:48 +0100

    adding test folder

---
+++ test/index.js
@@ -0,0 +1,19 @@
+'use strict'
+
+const Lab = require('lab')
+const lab = exports.lab = Lab.script()
+const server = require('../bin')
+const Code = require('code')
+
+lab.experiment('Routes', () => {
+  lab.test('Hello GET', done => {
+    const options = { method: 'GET', url: '/'}
+    server.inject(options, response => {
+      let result = response.result
+      Code.expect(result).to.equal('hello')
+      done()
+    })
+  })
+})
+
+
